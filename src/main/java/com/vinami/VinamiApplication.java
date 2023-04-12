48c56a84777839f568360711f8e42caecb0c1545
Wan Sim <0420syj@naver.com>
2023-04-12 20:31:20 +0900

    Initial commit

---
+++ src/main/java/com/vinami/VinamiApplication.java
@@ -0,0 +1,13 @@
+package com.vinami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinamiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinamiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinami/VinamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinamiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
